d25e7eaea752c626ea19ec2b541fe6bbbf806a9b
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-11-28 22:38:16 -0600

    optimize images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
-import logo from "/alphanails.jpg";
 
 const ShuffleHero = () => {
   return (
@@ -53,59 +52,59 @@ const squareData = [
   },
   {
     id: 3,
-    src: "https://images.unsplash.com/photo-1585201731775-0597e1be4bfb?q=80&w=2815&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphaone.jpg",
   },
   {
     id: 4,
-    src: "https://images.unsplash.com/photo-1590635023142-73c3d34f2805?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphatwo.jpg",
   },
   {
     id: 5,
-    src: "https://images.unsplash.com/photo-1605701249987-f0bb9b505d06?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphathree.jpg",
   },
   {
     id: 6,
-    src: "https://images.unsplash.com/photo-1618090673835-5f7a59837bfe?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphafour.jpg",
   },
   {
     id: 7,
-    src: "https://images.unsplash.com/photo-1616236062354-f668f56b2923?q=80&w=3085&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphafive.jpg",
   },
   {
     id: 8,
-    src: "https://images.unsplash.com/photo-1597066157837-5084c251fa34?q=80&w=2531&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 9,
-    src: "https://images.unsplash.com/photo-1566379680937-b375ba1b7691?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 10,
-    src: "https://images.unsplash.com/photo-1613945831677-383c19ad7721?q=80&w=2441&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 11,
-    src: "https://images.unsplash.com/photo-1605701249987-f0bb9b505d06?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 12,
-    src: "https://images.unsplash.com/photo-1618334703273-35144366beef?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 13,
-    src: "https://waykenrm.com/wp-content/uploads/2023/04/tubular-rivets.jpg",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 14,
-    src: "https://i.etsystatic.com/8664607/r/il/3dd9e0/3497425734/il_1588xN.3497425734_cq3v.jpg",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 15,
-    src: "https://waykenrm.com/wp-content/uploads/2023/04/types-of-rivets.jpg",
+    src: "hhttps://alphathreaded.vercel.app/alphaseven.jpg",
   },
   {
     id: 16,
-    src: "https://waykenrm.com/wp-content/uploads/2023/04/blind-rivets.jpg",
+    src: "https://alphathreaded.vercel.app/alphaseven.jpg",
   },
 ];
 
