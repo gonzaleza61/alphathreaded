542095e69dfd0afe2ea7ec158a58b2542e63f158
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-11-28 22:23:22 -0600

    bolts jpg

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -45,11 +45,11 @@ const shuffle = (array) => {
 const squareData = [
   {
     id: 1,
-    src: { logo },
+    src: "https://alphathreaded.vercel.app/alphanails.jpg",
   },
   {
     id: 2,
-    src: "https://images.unsplash.com/photo-1673833114586-f951168be369?q=80&w=2943&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://alphathreaded.vercel.app/alphabolts.jpg",
   },
   {
     id: 3,
