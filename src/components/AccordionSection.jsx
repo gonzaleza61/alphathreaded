c93cc23065e2ad4bcddd0f561555f0d140f06b48
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-11-30 23:25:28 -0600

    change

--- src/components/AccordionSection.jsx
+++ src/components/AccordionSection.jsx
@@ -58,7 +58,7 @@ const Panel = ({ open, setOpen, id, Icon, title, imgSrc, description }) => {
           {title}
         </span>
         <span className="block lg:hidden text-xl font-light">{title}</span>
-        <div className="w-6 lg:w-full aspect-square bg-red-700 text-white grid place-items-center">
+        <div className="w-6 lg:w-full aspect-square bg-red-600 text-white grid place-items-center">
           <Icon />
         </div>
         <span className="w-4 h-4 bg-white group-hover:bg-slate-50 transition-colors border-r-[1px] border-b-[1px] lg:border-b-0 lg:border-t-[1px] border-slate-200 rotate-45 absolute bottom-0 lg:bottom-[50%] right-[50%] lg:right-0 translate-y-[50%] translate-x-[50%] z-20" />
